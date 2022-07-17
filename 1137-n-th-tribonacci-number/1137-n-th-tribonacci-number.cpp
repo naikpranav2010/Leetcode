f46470e1f91fc7ccc4eccfbd67e96219731d71a8
naikpranav2010 <108756717+naikpranav2010@users.noreply.github.com>
2022-07-17 09:03:09 +0530

    Time: 0 ms (100.00%), Space: 6 MB (50.43%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        int arr[3] = {0, 1, 1};
+        
+        for(int i = 3; i <= n; i++)
+            arr[i % 3] = arr[0] + arr[1] + arr[2];
+        
+        return arr[n % 3];
+    }
+};
