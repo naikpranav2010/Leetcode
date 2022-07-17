c0857689a89cdda696b56dd3319ec91eecf77d3f
naikpranav2010 <108756717+naikpranav2010@users.noreply.github.com>
2022-07-17 08:01:48 +0530

    Time: 4 ms (22.52%), Space: 6.2 MB (15.07%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> mem(n + 1);
+        mem[0] = 1;
+        mem[1] = 1;
+        
+        for(int i = 2; i <= n; i++)
+            mem[i] = mem[i - 1] + mem[i - 2];
+        
+        return mem[n];
+    }
+};
