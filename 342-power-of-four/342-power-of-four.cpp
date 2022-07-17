6a6977ca8f685db9e951e586fbbfcd865d8d870e
naikpranav2010 <108756717+naikpranav2010@users.noreply.github.com>
2022-07-17 08:20:27 +0530

    Time: 8 ms (9.10%), Space: 5.9 MB (70.80%) - LeetHub

---
+++ 342-power-of-four/342-power-of-four.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n == 1)
+            return true;
+        
+        if(!n || n % 4)
+            return false;
+        
+        return isPowerOfFour(n / 4);
+    }
+};
