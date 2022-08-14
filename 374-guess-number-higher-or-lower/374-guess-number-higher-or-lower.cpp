ae5c9a010732b2c4948184d88daeb1f09e3e85a9
naikpranav2010 <108756717+naikpranav2010@users.noreply.github.com>
2022-08-14 17:09:06 +0530

    Time: 6 ms (5.04%), Space: 6 MB (23.05%) - LeetHub

--- 374-guess-number-higher-or-lower/374-guess-number-higher-or-lower.cpp
+++ 374-guess-number-higher-or-lower/374-guess-number-higher-or-lower.cpp
@@ -14,15 +14,15 @@ public:
             return start;
         
         int mid = start + (end - start) / 2;
-        int res = guess(mid);
+        // int res = guess(mid);
         
-        if(res == 0)
+        if(guess(mid) == 0)
             return mid;
         
-        if(res == -1)
+        if(guess(mid) == -1)
             return guessnum(start, mid - 1);
         
-        if(res == 1)
+        if(guess(mid) == 1)
             return guessnum(mid + 1, end);
         
         return 0;
