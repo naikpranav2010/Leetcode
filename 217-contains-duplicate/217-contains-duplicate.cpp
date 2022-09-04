35fa7c8810c0bb6a428ba10a828e3b4eb481a800
naikpranav2010 <108756717+naikpranav2010@users.noreply.github.com>
2022-09-04 15:17:28 +0530

    Time: 151 ms (50.52%), Space: 51.4 MB (52.07%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,41 +1,15 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int, bool> hash;
+        unordered_map<int, bool> mp;
         
         for(int i = 0; i < nums.size(); i++){
-            if(hash.find(nums[i]) != hash.end())
+            if(mp.find(nums[i]) != mp.end())
                 return true;
             
-            hash[nums[i]] = true;
+            mp[nums[i]] = true;
         }
         
         return false;
     }
-    
-//     bool exists(vector<int> &hash, int num){
-//         int i = 0, j = hash.size() - 1;
-        
-//         while(i < j){
-//             int mid = i + (j - i) / 2;
-            
-//             if(hash[mid] == num)
-//                 return true;
-            
-//             if(hash[mid] < num)
-//                 i = mid + 1;
-            
-//             else if(hash[mid] > num)
-//                 j = mid - 1;
-//         }
-        
-//         if(hash[j] == nums)
-//             return true;
-        
-//         return false;
-//     }
-    
-//     void insert(vector<int> &hash, int num){
-        
-//     }
 };
