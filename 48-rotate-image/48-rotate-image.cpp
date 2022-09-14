9754f3d86195df07c2ef3a1effc54ba110a05297
naikpranav2010 <108756717+naikpranav2010@users.noreply.github.com>
2022-09-14 16:45:23 +0530

    Time: 6 ms (23.33%), Space: 7.2 MB (33.37%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,20 +1,31 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& m) {
+        transpose(m);
+        reflect(m);
+        
+        return;
+    }
+    
+    void transpose(vector<vector<int>>& m){
         int n = m.size();
         
-        for(int i = 0; i <= (n - 1) / 2; i++){
-            int ri = i, rf = n - i - 1, ci = i, cf = n - i - 1, temp;
-            if(ri >= rf || ci >= cf)
-                break;
-            for(int j = 0; j < rf - ri; j++){
-                temp = m[ri][ci + j];
-                m[ri][ci + j] = m[rf - j][ci];
-                m[rf - j][ci] = m[rf][cf - j];
-                m[rf][cf - j] = m[ri + j][cf];
-                m[ri + j][cf] = temp;
+        for(int i = 0; i < n - 1; i++){
+            for(int j = i + 1; j < n; j++){
+                int temp = m[i][j];
+                m[i][j] = m[j][i];
+                m[j][i] = temp;
             }
         }
+        
+        return;
+    }
+        
+    void reflect(vector<vector<int>>& m){
+        int n = m.size();
+        
+        for(int i = 0; i < n; i++)
+            reverse(m[i].begin(), m[i].end());
         
         return;
     }
