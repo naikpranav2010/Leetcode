75aeb099559de12f3900182695b7862ff465a6ea
naikpranav2010 <108756717+naikpranav2010@users.noreply.github.com>
2022-09-14 14:36:20 +0530

    Time: 61 ms, Memory: 23 MB - LeetHub

---
+++ reverse-string/reverse-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        char temp;
+        int i = 0, j = s.size() - 1;
+        
+        while(i < j){
+            s[j] += s[i];
+            s[i] = s[j] - s[i];
+            s[j--] -= s[i++];
+        }
+        
+        return;
+    }
+};
