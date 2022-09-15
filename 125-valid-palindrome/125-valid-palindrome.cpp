bab0a079fdcefa6a1ca46a05067644aeb9fff2f7
naikpranav2010 <108756717+naikpranav2010@users.noreply.github.com>
2022-09-15 09:32:00 +0530

    Time: 18 ms (10.97%), Space: 7.4 MB (54.35%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -1,36 +1,22 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        clean(s);
+        int l = 0, r = s.size() - 1;
         
-        int n = s.size();
-        
-        for(int i = 0; i < n / 2; i++){
-            if(s[i] != s[n - 1 - i])
+        while(l < r){
+            if(!isalnum(s[l]))
+                l++;
+            else if(!isalnum(s[r]))
+                r--;
+            else if(tolower(s[l]) != tolower(s[r]))
                 return false;
+            else
+            {
+                l++;
+                r--;
+            }
         }
         
         return true;
-    }
-    
-    void clean(string &s){
-        for (int i = 0; i < s.size(); i++){
-            if(!((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= '0' && s[i] <= '9'))){
-                s.erase(s.begin() + i);
-                i--;
-            }
-                // s.erase(s.begin() + i);
-            // else
-            //     continue;
-            
-            // else if(s[i] < 'z')
-            //     s[i] -= 'a' + 'A';
-            
-            else if(s[i] <= 'Z')
-                s[i] = 'a' + (s[i] - 'A');
-            
-        }
-        cout<<s<<endl;
-        return;
     }
 };
