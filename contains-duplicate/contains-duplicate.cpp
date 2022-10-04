bff578e4a29b604d1fb8fc83a3b3a22156850916
naikpranav2010 <108756717+naikpranav2010@users.noreply.github.com>
2022-10-04 17:12:19 +0530

    Time: 93 ms, Memory: 51.5 MB - LeetHub

---
+++ contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> umap;
+        
+        for(int i  =0; i < nums.size(); i++){
+            if(umap.find(nums[i]) != umap.end())
+                return true;
+            
+            umap[nums[i]]++;
+        }
+        
+        return false;
+    }
+};
