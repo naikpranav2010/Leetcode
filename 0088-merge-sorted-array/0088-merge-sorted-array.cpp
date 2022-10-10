e0c2905246f6e8b701327300c7bb683326ee5526
naikpranav2010 <108756717+naikpranav2010@users.noreply.github.com>
2022-10-10 15:52:12 +0530

    Time: 7 ms (33.19%), Space: 9.1 MB (30.66%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -4,32 +4,17 @@ public:
         int i = m - 1, j = n - 1, k = m + n - 1;
         
         while(i >= 0 && j >= 0){
-            if(nums1[i] >= nums2[j]){
-                nums1[k] = nums1[i];
-                cout<<nums1[k]<<endl;
-                cout<<k<<endl;
-                k--;
-                i--;
-            }
+            if(nums1[i] >= nums2[j])
+                nums1[k--] = nums1[i--];
             
             else{
-                nums1[k] = nums2[j];
-                cout<<nums1[k]<<endl;
-                cout<<k<<endl;
-                k--;
-                j--;
+                nums1[k--] = nums2[j--];
             }
         }
         
-        if(i < 0){
-            while(j >= 0 && k >= 0){
-                nums1[k] = nums2[j];
-                cout<<nums1[k]<<endl;
-                cout<<k<<endl;
-                k--;
-                j--;
-            }
-        }
+        if(i < 0)
+            while(j >= 0 && k >= 0)
+                nums1[k--] = nums2[j--];
         
         // else if(j < 0){
         //     while(i >= 0)
