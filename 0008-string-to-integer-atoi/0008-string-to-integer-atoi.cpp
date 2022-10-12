60deadba6c31c422decc122d06c0053737092b22
naikpranav2010 <108756717+naikpranav2010@users.noreply.github.com>
2022-10-12 09:44:06 +0530

    Time: 7 ms (47.77%), Space: 7 MB (52.36%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int ans = 0, ptr = 0, isnegative = 1;
+        
+        while(ptr < s.size() && s[ptr] == ' '){
+                ptr++;
+        }
+        
+        if(s[ptr] == '-'){
+            isnegative -= 2;
+            ptr++;
+        }
+            
+        
+        else if(s[ptr] == '+'){
+            // isnegative++;
+            ptr++;
+        }
+        
+        while(ptr < s.size()){
+            if(isdigit(s[ptr])){
+                // cout<<"enter"<<endl;
+                if(ans > INT_MAX / 10 || ans == INT_MAX / 10 && s[ptr] - '0' > 7)
+                    return isnegative > 0 ? INT_MAX : INT_MIN;
+                
+                ans = ans * 10 + (s[ptr] - '0');
+                // ans = 42;
+                
+                ptr++;
+            }
+            
+            else 
+                break;
+        }
+        
+        return isnegative * ans;
+    }
+};
